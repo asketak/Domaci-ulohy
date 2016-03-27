fa97990fa55acfbbcebfcea148a6f8c9e2c582e5
asketak <simatomas@seznam.cz>
2016-03-27 23:45:04 +0200

    1/C, kresleni do radku

--- cviceni1/C.cpp
+++ cviceni1/C.cpp
@@ -6,7 +6,9 @@
 #include <cairo/cairo.h>
 #include <cassert>
 
-const int SPIRAL_IMAGE_EDGE_SIZE = 3;
+const int PIXEL_SIZE = 3; // one number/point in ulman spiral correspon to PIXEL_SIZE * PIXEL_SIZE rectangle in image
+const int SPIRAL_IMAGE_SIZE = 21; // How many rectangles wide the image will be
+const int SPIRAL_IMAGE_EDGE_SIZE = SPIRAL_IMAGE_SIZE*PIXEL_SIZE; // Acutal pixel wise size of image
 const int MAX_PRIME = SPIRAL_IMAGE_EDGE_SIZE * SPIRAL_IMAGE_EDGE_SIZE+1;
 const int CENTER_COORDINATE = (SPIRAL_IMAGE_EDGE_SIZE - 1) /2 ;
 
@@ -31,15 +33,29 @@ bool isPrime(int x){
 
 void write_spiral(std::array<bool, MAX_PRIME> points)
 {
-	for (int i = 1; i < MAX_PRIME; ++i)
+
+    cairo_surface_t *surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, SPIRAL_IMAGE_EDGE_SIZE, SPIRAL_IMAGE_EDGE_SIZE);
+    cairo_t *cr = cairo_create (surface); 
+
+    cairo_set_source_rgb (cr, 128, 128, 128);
+    cairo_paint(cr);
+
+    cairo_set_source_rgb (cr, 0, 0, 0);
+
+	for (int i = 1; i < SPIRAL_IMAGE_SIZE; ++i)
 	{
 		std::cout << "IT" << std::endl;
 		if (points[i])
 		{
+			cairo_rectangle (cr, (i-1)*PIXEL_SIZE, 0, PIXEL_SIZE, PIXEL_SIZE);
 			std::cout << "PRVOCISLO" << std::endl;
 		}
 		
 	}
+	cairo_fill (cr);
+    cairo_destroy (cr);
+    cairo_surface_write_to_png (surface, "spiral.png");
+    cairo_surface_destroy (surface);
 
 }
 
@@ -54,9 +70,9 @@ std::array<bool, MAX_PRIME> fillPrimes(){
 
 int main(int argc, char const *argv[])
 {
-	assert(SPIRAL_IMAGE_EDGE_SIZE%2==1);
+	assert(SPIRAL_IMAGE_SIZE%2==1);
 	std::array<bool, MAX_PRIME> primes = fillPrimes();
-	//write_spiral(primes);
+	write_spiral(primes);
 
 	return 0;
 }
