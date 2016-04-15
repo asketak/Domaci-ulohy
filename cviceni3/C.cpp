d98b8f32c4cb1097973629f80123a632b329f1ee
asketak <simatomas@seznam.cz>
2016-04-15 02:38:00 +0200

    3/C pentaflake donu, but ugly with magical constants

--- cviceni3/C.cpp
+++ cviceni3/C.cpp
@@ -86,7 +86,8 @@ void sierping_recursion(Turtle *turtle, int deepness, double current_step_size){
 	turtle->forward(current_step_size/2);
 
 	turtle->right(120);
-	}
+}
+
 void sierping_triangle( int deepness, std::string file_name){
 
 	double current_step_size = 256;
@@ -97,6 +98,50 @@ void sierping_triangle( int deepness, std::string file_name){
 	turtle.right(150);
 	sierping_recursion(&turtle, deepness,current_step_size);
 	turtle.save(file_name.c_str());
+}
+
+
+void pentaflake_recursion(Turtle *turtle, int deepness, double current_step_size){
+
+	const double angle = (360 / 5);
+	const double ang = 360 - 3*(180-angle);
+
+	if (deepness == 0)
+	{
+		for (int i = 0; i < 5; ++i)
+		{
+	turtle->pendown();
+			turtle->forward(current_step_size);
+			turtle->right(angle);
+	turtle->penup();
+		}
+			return;
+	}
+
+		double next_step_size = current_step_size * 1.9 / 5.0;
+	for (int i = 0; i < 5; ++i)
+	{
+		std::cout << angle << ":" << ang << ":" << current_step_size << std::endl;
+		pentaflake_recursion(turtle, deepness-1, next_step_size);
+		turtle->forward(current_step_size);
+		turtle->right(angle);
+	}
+	turtle->right(ang);
+	turtle->forward(next_step_size*1.65);
+	pentaflake_recursion(turtle, deepness-1, next_step_size);
+	turtle->back(next_step_size*1.65);
+	turtle->left(ang);
+}
+
+void pentaflake( int deepness, std::string file_name){
+
+	double current_step_size = 200;
+	double IMAGE_SIZE = current_step_size*10 ;
+
+	Turtle turtle(IMAGE_SIZE/2,IMAGE_SIZE/2,IMAGE_SIZE);
+	turtle.left(90);
+	pentaflake_recursion(&turtle, deepness,current_step_size);
+	turtle.save(file_name.c_str());
 
 }
 
@@ -105,6 +150,7 @@ int main(int argc, char const *argv[])
 	bush(10,"bush.png");
 	koch(3,"koch.png");
 	sierping_triangle(5,"sierping_triangle.png");
+	pentaflake(2,"pentaflake.png");
 
 	return 0;
 }
