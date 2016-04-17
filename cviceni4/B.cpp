b312d66269c1f3d26edc048b21aa7f88d4c91d76
asketak <simatomas@seznam.cz>
2016-04-17 13:49:02 +0200

    uhh, did the shape filling with reverse flood, my invented algo

--- cviceni4/B.cpp
+++ cviceni4/B.cpp
@@ -3,25 +3,18 @@
 #include <vector>
 #include <cairo/cairo.h>
 
-const int IMAGE_PIXEL_SIZE = 1000;
+const int IMAGE_PIXEL_SIZE = 250;
 
 void put_pixel(cairo_t *cr, int x, int y, double r, double g, double b){
 	cairo_set_source_rgb (cr, r, g, b);
-	cairo_rectangle(cr, x, y, 3, 3);
+	cairo_rectangle(cr, x, y, 1, 1);
 	cairo_fill (cr);
 }
 
 void drawline(int (&black_points)[IMAGE_PIXEL_SIZE][IMAGE_PIXEL_SIZE], std::pair<double,double> a, std::pair<double,double> b){
 	
-	std::cout << "DRAWLIN" << std::endl;
 	double direction = 0;
 	bool vertical_flag = false;
-	if (b.first < a.first)
-	{
-		std::pair<double,double> temp = a;
-		a = b;
-		b = temp;
-	}
 	if (b.first != a.first)
 	{
 		direction = (b.second - a.second) / ( b.first - a.first );
@@ -30,14 +23,18 @@ void drawline(int (&black_points)[IMAGE_PIXEL_SIZE][IMAGE_PIXEL_SIZE], std::pair
 	}
 	for (double x = a.first; x <= b.first; x+=0.01)
 	{
-		for (int y = a.second; y <= b.second; y++)
+		for (int y = std::min(a.second,b.second); y <= std::max(a.second,b.second); y++)
 		{
+			if (std::min(a.second,b.second) == b.second)
+			{
+				direction *= -1;
+			}
 			double truy = a.second + (direction*(x-a.first));
 				//std::cout << x << ":" << y << ":" << b.first << ":" << b.second << ":" << direction << ":" << truy << std::endl;
-	//			std::cout << y << ":" << truy << std::endl;
-			if ((y / truy < 1.05 && y / truy > 0.95) || (vertical_flag && x == a.first))
+				//std::cout << y << ":" << truy << std::endl;
+			if ((y / truy < 1.02 && y / truy > 0.98) || (vertical_flag && x == a.first))
 			{
-	//std::cout << "PIXEL" << std::endl;cr
+	//std::cout << "PIXEL" << std::endl;
 				black_points[(int)(x)][y]= 1;
 			} 
 		}
@@ -49,8 +46,8 @@ void	drawdata(cairo_t *cr, int (&black_points)[IMAGE_PIXEL_SIZE][IMAGE_PIXEL_SIZ
 	{
 		for (int y = 0; y < IMAGE_PIXEL_SIZE; ++y)
 		{
-			if (black_points[x][y] == 1)
-			{
+			if (black_points[x][y] <= 1)
+		{	
 				put_pixel(cr, x, y, 0,0,0);
 			}
 		}
@@ -58,51 +55,18 @@ void	drawdata(cairo_t *cr, int (&black_points)[IMAGE_PIXEL_SIZE][IMAGE_PIXEL_SIZ
 }
 
 
-void fill_shape(cairo_t *cr, int (&black_points)[IMAGE_PIXEL_SIZE][IMAGE_PIXEL_SIZE]){
-	for (int y = 0; y < IMAGE_PIXEL_SIZE; ++y)
+void fill_shape( int (&black_points)[IMAGE_PIXEL_SIZE][IMAGE_PIXEL_SIZE], int x , int y){
+	if (black_points[x][y] >= 1 || x == 1 || y == 1 || x +1 == IMAGE_PIXEL_SIZE || y +1 == IMAGE_PIXEL_SIZE)
 	{
-		bool inFlag = false;
-		bool lineFlag = false;
-		int switch_count = 0;
-		for (int xx = 0; xx < IMAGE_PIXEL_SIZE; ++xx)
-		{
-			if (black_points[xx][y] == 1)
-			{
-				lineFlag = true;
-			}
-			if (black_points[xx][y] == 0 && lineFlag)
-			{
-				inFlag = !inFlag;
-				lineFlag = false;
-				switch_count++;
-			}
-
-		}
-
-		inFlag = false;
-		lineFlag = false;
-
-		for (int x = 0; x < IMAGE_PIXEL_SIZE && switch_count>1; ++x)
-		{
-
-
-
-			if (black_points[x][y] == 1)
-			{
-				lineFlag = true;
-			}
-			if (black_points[x][y] == 0 && lineFlag)
-			{
-				inFlag = !inFlag;
-				lineFlag = false;
-			}
-			if (inFlag)
-			{
-				put_pixel(cr, std::floor(x), y, 0,0,0);
-			}
-		}
-
+		return;
 	}
+
+	black_points[x][y] = 2;
+	fill_shape(black_points,x+1,y);
+	fill_shape(black_points,x,y-1);
+	fill_shape(black_points,x-1,y);
+	fill_shape(black_points,x,y+1);
+
 }
 
 void draw_shape(cairo_t *cr, std::vector<std::pair<double,double>> points){
@@ -112,9 +76,11 @@ void draw_shape(cairo_t *cr, std::vector<std::pair<double,double>> points){
 	for (auto i = points.begin(); i != points.end()-1; ++i)
 	{
 		drawline(picture, *i,*(i+1));
+		drawline(picture, *(i+1),*(i));
 	}
 	drawline(picture, *points.begin(),*(points.end()-1));
-	fill_shape(cr,picture);
+	drawline(picture, *(points.end()-1),*points.begin());
+	fill_shape(picture,2,2);
 	drawdata(cr,picture);
 }
 
@@ -127,12 +93,13 @@ int main(int argc, char const *argv[])
 	cairo_paint(cr);
 	cairo_set_source_rgb (cr, 0, 0, 0);
 	std::vector<std::pair<double,double>> points;
-	points.push_back(std::make_pair<double,double>(50,50));
-	points.push_back(std::make_pair<double,double>(53,300));
-	points.push_back(std::make_pair<double,double>(200,300));
+	points.push_back(std::make_pair<double,double>(200,100));
+	points.push_back(std::make_pair<double,double>(150,70));
+	points.push_back(std::make_pair<double,double>(100,100));
+	points.push_back(std::make_pair<double,double>(150,50));
+//	points.push_back(std::make_pair<double,double>(100,300));
 	
 	draw_shape(cr, points);
-	//fill_shape(cr);
 
 	cairo_destroy (cr);
 	cairo_surface_write_to_png (surface, "circle.png");
