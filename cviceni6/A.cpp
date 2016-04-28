a96e92326e44a4292ccd2a9e82620ddbd416dc72
asketak <simatomas@seznam.cz>
2016-04-28 23:00:12 +0200

    6/A done, wow, such fractal, much chaos

--- cviceni6/A.cpp
+++ cviceni6/A.cpp
@@ -6,8 +6,11 @@
 
 
 const int PIXEL_SIZE = 2;
-const int NUMBER_OF_LINES = 8;
-const double RADIUS = 50;
+const int NUMBER_OF_LINES = 3;
+const double RADIUS = 200;
+const double FRACTION = 0.5;
+
+const int NUMBER_OF_ITERATIONS = 5000;
 
 const double IMAGE_SIZE = RADIUS*2*PIXEL_SIZE;
 const double IMAGE_PIXEL_SIZE = IMAGE_SIZE*1.1;
@@ -26,7 +29,7 @@ std::vector<Point> symetric_polygon(double number_of_sides, double radius ){
 	std::vector<Point> ret;
 
 	double x = IMAGE_SIZE/2;
-double 	y = IMAGE_SIZE/2;
+	double y = IMAGE_SIZE/2;
 	double angle =  360 /number_of_sides ; 
 	double direction = 180;
 	for (int i = 0; i < number_of_sides; ++i)
@@ -39,6 +42,31 @@ double 	y = IMAGE_SIZE/2;
 	return ret;
 }
 
+
+void chaos_game(cairo_t *cr, std::vector<Point> points, int iterations) {
+	std::vector<Point> tmp;
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(0,IMAGE_SIZE);
+	std::uniform_int_distribution<> point_sel(0,NUMBER_OF_LINES-1);
+
+	Point p = points[0];
+
+	for (int i = 0; i < iterations; ++i)
+	{
+			Point direction = points[point_sel(gen)];
+			double dest_x = std::abs(p.first - direction.first) * FRACTION + std::min(p.first,direction.first) ;
+			double dest_y = std::abs(p.second - direction.second) * FRACTION + std::min(p.second,direction.second) ;
+			std::cout << p.first << ":" << p.second << "____" << direction.first << ":" << direction.second << "___" << dest_x << ":" << dest_y << std::endl;
+			//cairo_move_to (cr, p.first, p.second);
+			put_pixel(cr,dest_x,dest_y,0,0,0);
+//			cairo_line_to (cr, dest_x,dest_y);
+	//		cairo_stroke (cr);
+			p.first = dest_x;
+			p.second = dest_y;
+	}
+}
+
 int main(int argc, char const *argv[])
 {
 	std::vector<Point> points = symetric_polygon(NUMBER_OF_LINES,RADIUS);
@@ -48,6 +76,7 @@ int main(int argc, char const *argv[])
 	cairo_set_source_rgb (cr, 1, 1, 1);
 	cairo_paint(cr);
 	cairo_set_source_rgb (cr, 0, 0, 0);
+	chaos_game(cr, points, NUMBER_OF_ITERATIONS);
 	for (auto i = points.begin(); i != points.end(); ++i)
 	{
 		put_pixel(cr,i->first,i->second, 0,0,0);
